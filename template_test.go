1baf1e9803e01ddc6da8ffce79db1a1d14fe1d6f
Dan Slimmon <dslimmon@hashicorp.com>
2021-04-01 19:04:48 +0000

    moved template definitions into functions
    to make the API more consistent; in at least one case we need to call
    .Funcs(), so we need functions rathehr than variables.

--- template_test.go
+++ template_test.go
@@ -60,7 +60,7 @@ func TestTemplateInputs(t *testing.T) {
 
 	tpl, err := template.New("test").Parse(`{{template "inputs" .}}`)
 	assert.Nil(err)
-	_, err = tpl.Parse(TemplateInputs)
+	AddTemplateInputs(tpl)
 	assert.Nil(err)
 
 	for i, tc := range testCases {
@@ -125,7 +125,7 @@ func TestTemplateOutputs(t *testing.T) {
 
 	tpl, err := template.New("test").Parse(`{{template "outputs" .}}`)
 	assert.Nil(err)
-	_, err = tpl.Parse(TemplateOutputs)
+	AddTemplateOutputs(tpl)
 	assert.Nil(err)
 
 	for i, tc := range testCases {
@@ -171,8 +171,7 @@ func TestTemplateTableOfContents(t *testing.T) {
 					Children: []StepTemplateData{},
 				},
 			},
-			Out: `
-- [Hello](#0-hello)
+			Out: `- [Hello](#0-hello)
 - [Goodbye](#1-goodbye)`,
 		},
 		testCase{
@@ -203,8 +202,7 @@ func TestTemplateTableOfContents(t *testing.T) {
 					Children: []StepTemplateData{},
 				},
 			},
-			Out: `
-- [Hello](#0-hello)
+			Out: `- [Hello](#0-hello)
     - [Hello child](#00-hello-child)
     - [Hello other child](#01-hello-other-child)
 - [Goodbye](#1-goodbye)`,
@@ -213,7 +211,7 @@ func TestTemplateTableOfContents(t *testing.T) {
 
 	tpl, err := template.New("test").Parse(`{{template "table_of_contents" .}}`)
 	assert.Nil(err)
-	_, err = tpl.Parse(TemplateTableOfContents)
+	AddTemplateTableOfContents(tpl)
 	assert.Nil(err)
 
 	for i, tc := range testCases {
@@ -480,11 +478,10 @@ body of grandchild 0`,
 
 	tpl, err := template.New("test").Parse(`{{template "step" .}}`)
 	assert.Nil(err)
+	AddTemplateStep(tpl)
 	template.Must(tpl.Parse(`{{define "inputs"}}INPUTS{{end}}`))
 	template.Must(tpl.Parse(`{{define "outputs"}}OUTPUTS{{end}}`))
 	template.Must(tpl.Parse(`{{define "table_of_contents"}}TABLE_OF_CONTENTS{{end}}`))
-	_, err = tpl.Parse(TemplateStep)
-	assert.Nil(err)
 
 	for i, tc := range testCases {
 		t.Logf("test case %d", i)
@@ -533,14 +530,14 @@ this is the description of my step`,
 		},
 	}
 
-	tpl, err := template.New("test").Parse(TemplateExecStep)
-	assert.Nil(err)
+	tpl := template.New("test")
+	AddTemplateExecStep(tpl)
 
 	for i, tc := range testCases {
 		t.Logf("test case %d", i)
 
 		var b bytes.Buffer
-		err = tpl.Execute(&b, tc.In)
+		err := tpl.Execute(&b, tc.In)
 		assert.Nil(err)
 		assert.Equal(tc.Out, b.String())
 	}
